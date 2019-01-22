c6a74b5644e163285b151d340043f6c456fed2d9
Reader8765 <bookreader8765@gmail.com>
2019-01-22 09:35:57 -0500

    Update pixi.js

--- pixi.js
+++ pixi.js
@@ -1,131 +1,130 @@
 const LEVEL1MAP = [["x"," "," "," "," "],["x"," "," "," "," "],["x"," "," "," "," "],[],[]]
 
-let app = new PIXI.Application({ height: 550, width: 650 });
-let resources = PIXI.loader.resources;
-
-//BOX definitions
-var boxes = { options: ["cellWidth", "cellHeight", "backgroundImage"] };
-var level = { selectedMaps: [], position: [0, 0], container: new PIXI.Container(), height: 350 };
-
-function newBox(name, map, opts) {
-  boxes[name] = {};
-  boxes[name].map = map;
-  if (opts)
-    for (opt of boxes.options)
-      boxAssign(name, opt, opts);
-}
-
-function boxAssign(name, opt, opts) {
-  if (opts[opt]) {
-    boxes[name][opt] = opts[opt];
-  }
-}
-
-function selectBox(name) {
-  if (boxes[name])
-    level.selectedMaps.push(name);
-  else
-    throw "ERR: " + name + " is not a box";
-  if (boxes[name].backgroundImage)
-    level.container.addChild(new PIXI.Sprite(boxes[name].backgroundImage));
-  boxes[name].map.map((row, y) => {
-    row.map((val, x) => {
-      if (val == "x") {
-        var graphics = new PIXI.Graphics();
-
-        graphics.beginFill(0xFFFF00);
-
-        graphics.drawRect(x * 20, y * 20, 20, 20);
-
-        app.stage.addChild(graphics);
-      }
-    });
-  });
-}
-
-function removeBox() {
-
-}
-
-function detect(x, y, width, height) {
-
-}
-
-app.stage.addChild(level.container);
-
-const BOX = { newBox, selectBox, removeBox, detect };
-//end
-
+let app = new PIXI.Application({ height: 400, width: 400 });
+let resources;
 document.body.appendChild(app.view);
 app.renderer.backgroundColor = 0xa7efff;
 
-console.log("\n")
-
 PIXI.loader
-  .add("images/character.json")
-  .add("images/level1.png")
+  .add("playerJSON","images/sprites/playerSprite/sprite.json")
   .load(setup);
 
+class Global {
+  constructor(shapes){
+    this.shapes = shapes;
+    shapes.map(shape=>{
+      var graphics = new PIXI.Graphics();
+      graphics.beginFill(0xFFFF00);
+      graphics.drawRect(shape.x, shape.y, shape.width, shape.height);
+      app.stage.addChild(graphics);
+    });
+    this.users = {};
+    this.keyCheck = {};
+    this.update = this.update.bind(this);
+  }
+  update() {
+    Object.keys(this.users).map(user => {
+      this.users[user].update();
+    });
+  }
+  newUser(name, json, image, x, y, keys) {
+    var sprite = new PIXI.Sprite(resources[json].textures[image]);
 
-let charTextures, player;
-
-function setup() {
-  BOX.newBox("level1", LEVEL1MAP);
-  BOX.selectBox("level1");
-
-  charTextures = resources["images/character.json"].textures;
-
-  player = new PIXI.Sprite(
-    charTextures["charStopped"]
-  );
-
-  player.move = {
-    direction: "down",
-    moving: false,
-    move: () => {
-      if (typeof this.tick == "undefined") {
-        this.tick = 0;
-      }
-      if (this.moving) {
-        switch (this.direction) {
-          case "down":
-            player.x -= 5;
+    var textures = resources[json].textures;
+    this.users[name] = new Player(name, sprite, x, y, textures);
+    this.users[name].useKeys(keys[0],keys[1],keys[2],keys[3]);
+  }
+  defineKeys() {
+    document.addEventListener("keydown", e => {
+      Object.keys(this.keyCheck).map(user => {
+        var userKeys = this.keyCheck[user];
+        var userObj = this.users[user];
+        if (e.key == userKeys.up) {
+          userObj.direction = "up"
+          userObj.moving = true;
+        } else if (e.key == userKeys.left) {
+          userObj.direction = "left";
+          userObj.moving = true;
+        } else if (e.key == userKeys.down) {
+          userObj.direction = "down";
+          userObj.moving = true;
+        } else if (e.key == userKeys.right) {
+          userObj.direction = "right";
+          userObj.moving = true;
         }
-      } else {
-        tick = 0;
+      });
+    });
+    document.addEventListener("keyup", e => {
+      Object.keys(this.keyCheck).map(user => {
+        var userKeys = this.keyCheck[user];
+        var userObj = this.users[user];
+        if (e.key == userKeys.up && userObj.direction == "up") {
+          this.users[user].moving = false;
+        } else if (e.key == userKeys.left && userObj.direction == "left") {
+          this.users[user].moving = false;
+        } else if (e.key == userKeys.down && userObj.direction == "down") {
+          this.users[user].moving = false;
+        } else if (e.key == userKeys.right && userObj.direction == "right") {
+          this.users[user].moving = false;
+        }
+      });
+    });
+  }
+}
+
+class Entity {
+  constructor(name, sprite, x, y, textures) {
+    this.name = name;
+    this.sprite = sprite;
+    this.sprite.x = x;
+    this.sprite.y = y;
+    this.textures = textures;
+    this.direction = "down";
+    this.vector = [0,0];
+    this.moving = false;
+    app.stage.addChild(this.sprite);
+  }
+  checkCollision(objects) {
+    
+  }
+}
+
+class AI extends Entity {
+}
+
+class Player extends Entity {
+  useKeys(up, left, down, right) {
+    Game.keyCheck[this.name] = {up: up, left: left, down: down, right: right};
+    Game.defineKeys();
+  }
+  update() {
+    if (this.moving) {
+      switch (this.direction) {
+        case "up":
+          this.sprite.y -= 3;
+          break;
+        case "left":
+          this.sprite.x -= 3;
+          break;
+        case "down":
+          this.sprite.y += 3;
+          break;
+        case "right":
+          this.sprite.x += 3;
+          break;
       }
     }
   }
-
-  player.height = 85;
-  player.width = 35;
-
-  app.stage.addChild(player);
-  app.ticker.add(delta => gameLoop(delta));
 }
 
-function activateKeys() {
-  document.addEventListener("keydown", e => {
-    if (e.key.slice(0, 5) == "Arrow") {
-      player.move.direction = e.key.slice(5).toLowerCase();
-      player.move.moving = true;
-    }
-  });
+function setup() {
+  resources = PIXI.loader.resources;
+  const shapes = [
+    {x:75,y:0,width:50,height:250}
+  ]
 
-  document.addEventListener("keyup", e => {
-    if (e.key.slice(5).toLowerCase() == player.move.direction) {
-      player.move.moving = false;
-    }
-  });
-}
+  Game = new Global(shapes);
+  Game.newUser("User1","playerJSON","downStopped",50,50,["ArrowUp","ArrowLeft","ArrowDown","ArrowRight"]);
 
-activateKeys();
-
-async function gameLoop() {
-    player.move.move();
-    updateStatus(detect());
-}
-
-async function updateStatus(status) {
-
+  app.ticker.add(()=>Game.update());
 }
